ee3f5e743e7455fae93e816b5c3cca0e1b59acdf
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-04-26 19:04:27 +0900

    ﾕｰｻﾞｰ管理機能 修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,            presence: true 
-  validates :first_name,          presence: true, 
-                                  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
-  validates :last_name,           presence: true, 
-                                  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
-  validates :first_name_furigana, presence: true, 
-                                  format: { with: /\A[ァ-ヶー－]+\z/ } 
-  validates :last_name_furigana,  presence: true, 
-                                  format: { with: /\A[ァ-ヶー－]+\z/ } 
-  validates :birth_date,          presence: true 
+  with_options presence: true do
+    validates :nickname            
+    validates :first_name,          format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }  
+    validates :last_name,           format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
+    validates :first_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_furigana,  format: { with: /\A[ァ-ヶー－]+\z/ } 
+    validates :birth_date
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 
   has_many :items
-  has_many :buys   
 end
