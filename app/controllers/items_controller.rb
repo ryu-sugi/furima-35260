384289952e5704c3c5a3470f9fbbfb5175b86a6d
ryu-sugi <81607989+ryu-sugi@users.noreply.github.com>
2021-05-01 22:21:52 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: :destroy
+
 
   def new
     @item = Item.new
@@ -22,9 +24,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
  private
 
   def item_params
    params.require(:item).permit(:item_name, :image, :category_id, :condition_id, :shipping_area_id, :shipping_day_id, :shipping_cost_id, :value, :description).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user
+      redirect_to root_path
+    end
   end
 end
